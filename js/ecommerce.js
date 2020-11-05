65f786049d573e5ef6592b185d0b017b9913aaaf
Leandro Leite <leandro.leite@concrete.com.br>
2020-11-05 19:31:14 -0300

    Limpa conteúdo de site antigo

--- js/ecommerce.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $(".JS-depoimento").click(function(evento) {
-    evento.preventDefault();
-    $(".JS-depoimentoTexto").hide();
-    div_id = $(this).attr("related-div");
-    $(div_id).show();
-  });
-});
